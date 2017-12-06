0c5626c7b2a1d6dfba6a0e6b43dc697f32e6c4ed
downthesun <33359239+downthesun@users.noreply.github.com>
2017-12-06 14:02:38 -0500

    Update Ch.2 stock_transaction.py

--- Chapter 2/Ch.2 stock_transaction.py
+++ Chapter 2/Ch.2 stock_transaction.py
@@ -1,23 +1,16 @@
 shares_purchased = int(x=2000)
-
 per_share = float(40.00)
-
 percentage = float(0.03)
 
 total_paid = shares_purchased*per_share
-
 commission_paid = (total_paid*percentage)
 
 shares_sold = int(x=2000)
-
 sold_for = float(42.75)
-
 total_sold = shares_sold*sold_for
-
 commission_sold = (total_sold*percentage) 
 
 total_commission = commission_paid+commission_sold
-
 profit = total_sold-total_paid-total_commission
 
 print('Amount paid for the stock: $', total_paid )
